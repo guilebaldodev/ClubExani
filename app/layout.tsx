ed8b0cd88dfdfa46dc97998e89e61f487ac76123
Guilebaldo <guilebaldodev@gmail.com>
2025-10-15 15:53:50 -0600

    google Tracking bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,14 +28,14 @@ export default function RootLayout({
         <InitUser></InitUser>
         {children}
 
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HV3B8VJRKF"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', ''G-HV3B8VJRKF');
+              gtag('config', 'G-HV3B8VJRKF');
             `,
           }}
         />
